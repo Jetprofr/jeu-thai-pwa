f57fc3def20003c7547aacc6094f745d2bb9c5e5
Jetprofr <karim.bendahou@gmail.com>
2025-07-02 15:20:31 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,7 @@
-const cacheName = "nombres-thai-v1";
-const filesToCache = [
-  "/jeu_nombres_thai_pwa.html",
-  "/manifest.json",
-  "/icon.png"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match("index.html");
     })
   );
 });
